9960ccb6a6d7bd5a748298613ff0832b6cfd0b4d
livio finos <livio@stat.unipd.it>
2022-01-05 13:49:26 +0100

    ...

--- R/acca_package.R
+++ R/acca_package.R
@@ -1,6 +1,6 @@
-#' A Canonical Correlation Analysis (acca)
+#' A Canonical Correlation Analysis with Inferential Guaranties
 #'
-#' @description It performs Canonical Correlation Analysis and provides inferential guaranties on the correlation components. The p-values are computed following the resampling method developed in Winkler, A. M., Renaud, O., Smith, S. M., & Nichols, T. E. (2020). Permutation inference for canonical correlation analysis. NeuroImage, 220, 117065. https://doi.org/10.1016/j.neuroimage.2020.117065. Furthermore, it provides plotting tools to visualize the results.
+#' @description It performs Canonical Correlation Analysis and provides inferential guaranties on the correlation components. The p-values are computed following the resampling method developed in Winkler, A. M., Renaud, O., Smith, S. M., & Nichols, T. E. (2020). Permutation inference for canonical correlation analysis. NeuroImage, <doi:10.1016/j.neuroimage.2020.117065>. Furthermore, it provides plotting tools to visualize the results.
 #' @examples
 #' set.seed(1)
 #' X=matrix(rnorm(500),100,5)
