bb0540aebcc378839d974484e74f3df087f02a6d
Mac di Edoardo <macbookm4@Mac.homenet.telecomitalia.it>
2026-04-01 11:50:11 +0200

    Implementazione nuove opzioni nella funzione cc_inference: di dedault, la funzione esegue un test basato su procedure di ricampionamento, impostando come predefinite la residualizzazione di Huh-Jhun, le rotazioni come schema di rimescolamento dei dati e la statistica test di Roy. L'utente ha comunque la flessibilità di selezionare approcci alternativi sia per la residualizzazione che per il ricampionamento, nonchè di optare per la statistica di Wilks. Infine, per maggiore completezza metodologica, è inclusa l'opzione per un test inferenziale parametrico classico, il quale prescinde dalle procedure di rimescolamento e si avvale esclusivamente della statistica di Wilks.

--- R/cc_inference.R
+++ R/cc_inference.R
@@ -22,49 +22,74 @@
 #' mod
 #'@export
 #'
-cc_inference <-  function(mod,B=100, stat_test = "Roy", alpha_max=.5,numb_cc=NULL,resamp_type="sign-flip",light=FALSE){
+cc_inference <-  function(mod,B=1000,stat_test="Roy",alpha_max=.5,numb_cc=NULL,resamp_type="rotation",light=FALSE,test_type="resampling"){
   mod$call$cc_inference=match.call()
   n=nrow(mod$data$X)
-  resamp_type=match.arg(resamp_type,c("sign-flip","permutation"))
-  stat_test=match.arg(stat_test,c("Roy","Wilks"))
+  resamp_type=match.arg(resamp_type,c("rotation","sign-flip","permutation"))
+  test_type=match.arg(test_type,c("resampling","parametric"))
+  if(test_type=="resampling") {
+    stat_test=match.arg(stat_test, c("Roy","Wilks")) 
+  } else if(test_type=="parametric") {
+    stat_test="Wilks"
+  }
   if(is.null(numb_cc)) numb_cc=length(mod$cor)
   
-  
-  ####################
   if(!light) {
-    mod=.cc_inference_orthogonal(mod,B,stat_test, alpha_max,numb_cc,resamp_type)
-  } else   if(light) {
-    mod=.cc_inference_residuals(mod,B,stat_test, alpha_max,numb_cc,resamp_type)
+    if(test_type=="resampling"){
+      mod=.cc_inference_orthogonal(mod,B,stat_test,alpha_max,numb_cc,resamp_type)
+    }
+    else if(test_type=="parametric"){
+      mod=.cc_inference_parametric(mod,alpha_max,numb_cc)
+    }
+  } 
+  else if(light){
+    if(test_type=="resampling"){
+      mod=.cc_inference_residuals(mod,B,stat_test,alpha_max,numb_cc,resamp_type)
+    }
+    else if(test_type=="parametric"){
+      mod=.cc_inference_parametric(mod,alpha_max,numb_cc)
+    }
   }
-  ##################
-
   return(mod)
 }
 
+####################
 
-#############
-
-.cc_inference_orthogonal <- function(mod,B,stat_test, alpha_max,numb_cc,resamp_type){
+.cc_inference_orthogonal <- function(mod,B,stat_test,alpha_max,numb_cc,resamp_type){
   resid_matrix <- function(Zy)
     residualizing_matrix(Zy)$Q
-  if(resamp_type=="sign-flip"){
-    .permute <- function(X,Qx,nred){
-      t(Qx*(1-2*rbinom(nred,1,.5))) %*%X # X=Qx%*%mod$data$X
+  
+  if(resamp_type=="rotation") {
+    .permute <- function(X, Qx, nred) {
+      M = matrix(rnorm(nred*nred),nrow=nred,ncol=nred)
+      O = qr.Q(qr(M,LAPACK = FALSE))
+      flipsign = which(rbinom(nred, 1, 0.5) == 1)
+      if(length(flipsign) > 0) O[, flipsign] = -O[, flipsign]
+      t(O%*%Qx)%*%X
     }
-  } else if(resamp_type=="permutation"){
-    .permute <- function(X,Qx,nred){
-      t(Qx[sample(nred),]) %*%X # X=Qx%*%mod$data$X
+  } else if(resamp_type=="sign-flip") {
+    .permute <- function(X,Qx,nred) {
+      t(Qx*(1-2*rbinom(nred,1,.5)))%*%X # X=Qx%*%mod$data$X
+    }
+  } else if(resamp_type=="permutation") {
+    .permute <- function(X,Qx,nred) {
+      t(Qx[sample(nred),])%*%X # X=Qx%*%mod$data$X
     }
   }
-  perm_and_cc=function(X,Y,Qx,Qy,nredx,nredy,stat_test){
-    ccp=.cc_core(.permute(X,Qx,nredx), .permute(Y,Qy,nredy),numb_cc = 0)
-    ccp$cor[1]
+  
+  perm_and_cc <- function(X,Y,Qx,Qy,nredx,nredy,stat_test){
+    ccp=.cc_core(.permute(X,Qx,nredx),.permute(Y,Qy,nredy),numb_cc = 0)
+    if (stat_test=="Roy") {
+      return(ccp$cor[1]) 
+    } else if (stat_test=="Wilks") {
+      return(-sum(log(1-ccp$cor^2)))
+    }
   }
   
   if(is.null(mod$data$Zx)) {
     Qx=diag(nrow(mod$data$X))
     X=mod$data$X
-  } else{
+  } else {
     Qx=resid_matrix(mod$data$Zx)
     X=Qx%*%mod$data$X
   }
@@ -72,7 +97,7 @@ cc_inference <-  function(mod,B=100, stat_test = "Roy", alpha_max=.5,numb_cc=NUL
   if(is.null(mod$data$Zy)) {
     Qy=diag(nrow(mod$data$Y))
     Y=mod$data$Y
-  } else{
+  } else {
     Qy=resid_matrix(mod$data$Zy)
     Y=Qy%*%mod$data$Y
   }
@@ -80,75 +105,123 @@ cc_inference <-  function(mod,B=100, stat_test = "Roy", alpha_max=.5,numb_cc=NUL
   nredx=nrow(Qx)
   nredy=nrow(Qy)
   
-  
   mod$p_values=rep(1,length(mod$cor))
-  attr(mod$p_values,which = "B")=B
+  attr(mod$p_values,which="B")=B
   
-  
-  n_nuisx=min(0,ncol(mod$data$Zx))
-  n_nuisy=min(0,ncol(mod$data$Zy))
+  n_nuisx = if(is.null(mod$data$Zx)) 0 else ncol(mod$data$Zx)
+  n_nuisy = if(is.null(mod$data$Zy)) 0 else ncol(mod$data$Zy)
   Zx=cbind(mod$data$Zx,mod$scores$xscores[,1:numb_cc])
   Zy=cbind(mod$data$Zy,mod$scores$yscores[,1:numb_cc])
   
-  for(i in 1:numb_cc){
-    mod$p_values[i]=(sum(replicate(B,perm_and_cc(X,Y,Qx,Qy,nredx,nredy))>=mod$cor[i])+1)/(B+1)
-    if(mod$p_values[i]>= alpha_max) return(mod)
-    Qx=resid_matrix(Zx[,1:(n_nuisx+i), drop = FALSE])
-    Qy=resid_matrix(Zy[,1:(n_nuisy+i), drop = FALSE])
+  for(i in 1:numb_cc) {
+    if (stat_test=="Roy") {
+      obs_stat=mod$cor[i]
+    } else if (stat_test=="Wilks") {
+      obs_stat=-sum(log(1-mod$cor[i:length(mod$cor)]^2))
+    }
+    perm_stats=replicate(B,perm_and_cc(X,Y,Qx,Qy,nredx,nredy,stat_test))
+    mod$p_values[i]=(sum(perm_stats >= obs_stat)+1)/(B+1)
+    if(mod$p_values[i] >= alpha_max) return(mod)
+    
+    Qx=resid_matrix(Zx[,1:(n_nuisx+i),drop=FALSE])
+    Qy=resid_matrix(Zy[,1:(n_nuisy+i),drop=FALSE])
     X=Qx%*%mod$data$X
     Y=Qy%*%mod$data$Y
   }
- 
+  
   return(mod)
 }
 
 #############
 
-.cc_inference_residuals <- function(mod,B,stat_test, alpha_max,numb_cc,resamp_type){
+.cc_inference_residuals <- function(mod,B,stat_test,alpha_max,numb_cc,resamp_type){
   n=nrow(mod$data$X)
-  if(resamp_type=="sign-flip"){
-    .permute <- function(X,n){
+  
+  if(resamp_type=="rotation") {
+    .permute <- function(X,n) {
+      M=matrix(rnorm(n*n),nrow=n,ncol=n)
+      O = qr.Q(qr(M, LAPACK = FALSE))
+      flipsign = which(rbinom(n, 1, 0.5) == 1)
+      if(length(flipsign) > 0) O[, flipsign] = -O[, flipsign]
+      O%*%X
+    }
+  } else if(resamp_type=="sign-flip") {
+    .permute <- function(X,n) {
       (1-2*rbinom(n,1,.5))*X # X=IH%*%mod$data$X
     }
-  } else if(resamp_type=="permutation"){
-    .permute <- function(X,n){
+  } else if(resamp_type=="permutation") {
+    .permute <- function(X,n) {
       X[sample(n),] # X=IH%*%mod$data$X
     }
-  }
+  } 
   
-  perm_and_cc=function(X,Y,n,stat_test){
-    ccp=.cc_core(.permute(X,n), Y,numb_cc =  0)
-    ccp$cor[1]
+  perm_and_cc <- function(X,Y,n,stat_test) {
+    ccp=.cc_core(.permute(X,n),Y,numb_cc=0)
+    if (stat_test=="Roy") {
+      return(ccp$cor[1]) 
+    } else if (stat_test=="Wilks") {
+      return(-sum(log(1-ccp$cor^2)))
+    }
   }
   
   if(is.null(mod$data$Zx)) {
     X=mod$data$X
-  } else{
+  } else {
     X=residualize(mod$data$X,mod$data$Zx)
   }
   
   if(is.null(mod$data$Zy)) {
     Y=mod$data$Y
-  } else{
+  } else {
     Y=residualize(mod$data$Y,mod$data$Zy)
   }
   
-  
   mod$p_values=rep(1,length(mod$cor))
-  attr(mod$p_values,which = "B")=B
+  attr(mod$p_values,which="B")=B
   
-  
-  n_nuisx=min(0,ncol(mod$data$Zx))
-  n_nuisy=min(0,ncol(mod$data$Zy))
+  n_nuisx = if(is.null(mod$data$Zx)) 0 else ncol(mod$data$Zx)
+  n_nuisy = if(is.null(mod$data$Zy)) 0 else ncol(mod$data$Zy)
   Zx=cbind(mod$data$Zx,mod$scores$xscores[,1:numb_cc])
   Zy=cbind(mod$data$Zy,mod$scores$yscores[,1:numb_cc])
   
-  for(i in 1:numb_cc){
-    mod$p_values[i]=(sum(replicate(B,perm_and_cc(X,Y,n))>=mod$cor[i])+1)/(B+1)
-    if((mod$p_values[i]>= alpha_max) | (i== numb_cc)) return(mod)
-    X=residualize(mod$data$X,Zx)
-    Y=residualize(mod$data$Y,Zy)
+  for(i in 1:numb_cc) {
+    if (stat_test=="Roy") {
+      obs_stat=mod$cor[i]
+    } else if (stat_test=="Wilks") {
+      obs_stat=-sum(log(1-mod$cor[i:length(mod$cor)]^2))
+    }
+    perm_stats <- replicate(B, perm_and_cc(X,Y,n,stat_test))
+    mod$p_values[i]=(sum(perm_stats >= obs_stat)+1)/(B+1)
+    if(mod$p_values[i] >= alpha_max) return(mod)
+    
+    X = residualize(mod$data$X,Zx[,1:(n_nuisx+i),drop=FALSE])
+    Y = residualize(mod$data$Y,Zy[,1:(n_nuisy+i),drop=FALSE])
+    
   }
-  
   return(mod)
 }
+
+####################
+
+.cc_inference_parametric <- function(mod,alpha_max,numb_cc){
+  
+  n=nrow(mod$data$X)
+  r = if(is.null(mod$data$Zx)) 0 else qr(mod$data$Zx)$rank
+  n_eff=n-r
+  p=ncol(mod$data$Y)
+  q=ncol(mod$data$X)
+  bartlett=-(n_eff-1-1/2*(p+q+1))
+  
+  mod$p_values=rep(1,length(mod$cor))
+  attr(mod$p_values,which ="test_type")="parametric"
+  
+  for (i in 1:numb_cc) {
+    lambda=log(prod(1-(mod$cor[i:length(mod$cor)])^2))
+    df=(p-i+1)*(q-i+1)
+    mod$p_values[i]=pchisq(bartlett*lambda,df,lower.tail=FALSE)
+    if(mod$p_values[i] >= alpha_max) return(mod)
+  }
+  return(mod)
+}
+
+####################
